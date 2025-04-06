d8b9655dd3fe74b8e4f47d0c8ac730e65446fd32
AndresRodriguez-coder <rodriguezavilaandresdavid@gmail.com>
2025-04-06 17:33:26 -0500

    conexion django con las bases de datos

---
+++ con_post/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ConPostConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'con_post'
---
+++ con_post/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Producto
+from .forms import ProductoForm
+
+def listar_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'mi_app/listar_productos.html', {'productos': productos})
+
+def crear_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'mi_app/crear_producto.html', {'form': form})
+
+def editar_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'mi_app/editar_producto.html', {'form': form})
+
+def eliminar_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    if request.method == 'POST':
+        producto.delete()
+        return redirect('listar_productos')
+    return render(request, 'mi_app/eliminar_producto.html', {'producto': producto})
